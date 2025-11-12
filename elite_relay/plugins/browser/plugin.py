127cbead754044cb014e4bbfc936b15e72dc0d9c
Andrew Mickael <andrew.mickael@gmail.com>
2025-11-12 17:41:14 -0600

    Add open_method_code property to BrowserOptions

--- elite_relay/plugins/browser/plugin.py
+++ elite_relay/plugins/browser/plugin.py
@@ -1,7 +1,7 @@
 import typing as t
 import webbrowser
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, computed_field
 
 from elite_relay.plugins.base import BasePlugin
 
@@ -20,12 +20,17 @@ class BrowserOptions(BaseModel):
     focus: bool = False
     open_method: OpenMethod = 'default'
 
+    @computed_field
+    @property
+    def open_method_code(self) -> int:
+        return _open_method_map[self.open_method]
+
 
 class BrowserPlugin(BasePlugin):
     def open(self):
         options = BrowserOptions.model_validate(self.config.options)
         webbrowser.open(
             url=self.format_string(options.url),
-            new=_open_method_map[options.open_method],
+            new=options.open_method_code,
             autoraise=options.focus,
         )
