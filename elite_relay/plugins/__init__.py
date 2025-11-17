ef99928f73752212479f525d3e51af8e836f5691
Andrew Mickael <andrew.mickael@gmail.com>
2025-11-17 09:28:31 -0600

    Add more docs

--- elite_relay/plugins/__init__.py
+++ elite_relay/plugins/__init__.py
@@ -3,6 +3,7 @@ from elite_relay.plugins.registry import PluginRegistry
 from .browser import BrowserPlugin
 from .clipboard import ClipboardPlugin
 from .http import HttpPlugin
+from .pushover import PushoverPlugin
 
 registry = PluginRegistry()
 
@@ -12,3 +13,4 @@ registry = PluginRegistry()
 registry.register('http', HttpPlugin)
 registry.register('browser', BrowserPlugin)
 registry.register('clipboard', ClipboardPlugin)
+registry.register('pushover', PushoverPlugin)
